e5878dda67286190fa051bf6d8ae97bc2e57f8ca
Ryan H <littlekishi@gmail.com>
2019-01-25 15:38:48 -0600

    Possible fix to the 'new' empower

--- empowerify.js
+++ empowerify.js
@@ -1,6 +1,12 @@
-window.onbeforeunload = closingCode;
+function checkIfNewEmpower(){
+	if (window.location.search == "?iCtrl=STUDENT_BASE_HOME_CONTROL") {
+		window.location.replace('https://plano.empowerlearning.net/iFrame.aspx?iCtrl=PLAYLIST_HOME_CLASS');
+	}
+}
 function closingCode(){
    var message = "Are you sure you want to navigate away from this page?\n\nYou have started writing or editing a post.\n\nPress OK to continue or Cancel to stay on the current page.";
 	if (confirm(message)) return true;
 	else return false;
 }
+window.onbeforeunload = closingCode;
+window.onload = checkIfNewEmpower;

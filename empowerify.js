4917e984589356adf03aebc757d9bfeccbf1528f
Ryan H <littlekishi@gmail.com>
2019-02-13 15:45:59 -0600

    options!

--- empowerify.js
+++ empowerify.js
@@ -1,12 +1,45 @@
+var redirect = true;
+var goBack   = true;
+
+
+chrome.storage.sync.get({
+    redirect: true,
+    goBack: true
+}, function(items) {
+    redirect = items.redirect;
+    goBack   = items.goBack;
+});
+
+
+
 function checkIfNewEmpower(){
-	if (window.location.search == "?iCtrl=STUDENT_BASE_HOME_CONTROL") {
-		window.location.replace('https://plano.empowerlearning.net/iFrame.aspx?iCtrl=PLAYLIST_HOME_CLASS');
+	chrome.storage.sync.get({
+	    redirect: true,
+	    goBack: true
+	}, function(items) {
+	    redirect = items.redirect;
+	    goBack   = items.goBack;
+	});
+	if (redirect == true) {
+		if (window.location.search == "?iCtrl=STUDENT_BASE_HOME_CONTROL") {
+			window.location.replace('https://plano.empowerlearning.net/iFrame.aspx?iCtrl=PLAYLIST_HOME_CLASS');
+		}
 	}
 }
 function closingCode(){
-   var message = "Are you sure you want to navigate away from this page?\n\nYou have started writing or editing a post.\n\nPress OK to continue or Cancel to stay on the current page.";
-	if (confirm(message)) return true;
-	else return false;
+	chrome.storage.sync.get({
+	    redirect: true,
+	    goBack: true
+	}, function(items) {
+	    redirect = items.redirect;
+	    goBack   = items.goBack;
+	});
+	var message = "Are you sure you want to navigate away from this page?\n\nYou have started writing or editing a post.\n\nPress OK to continue or Cancel to stay on the current page.";
+	
+	if (goBack == true) {
+		if (confirm(message)) return true;
+		return false;
+	}
 }
 window.onbeforeunload = closingCode;
 window.onload = checkIfNewEmpower;
---
+++ options.js
@@ -0,0 +1,31 @@
+// Saves options to chrome.storage
+function save_options() {
+  var redirect = document.getElementById('redirect').checked;
+  var goBack =   document.getElementById('goBack').checked;
+  chrome.storage.sync.set({
+    redirect: redirect,
+    goBack: goBack
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  chrome.storage.sync.get({
+    redirect: true,
+    goBack: true
+  }, function(items) {
+    document.getElementById('redirect').checked = items.redirect;
+    document.getElementById('goBack').checked	= items.goBack;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);

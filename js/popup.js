159d7049392603af1646e5f56d12ae4f639dbff6
Daniel <dwbrub@gmail.com>
2019-02-26 13:32:53 -0600

    Idk if this works.
    what is github
    dont commit
    commit no more

--- js/popup.js
+++ js/popup.js
@@ -3,11 +3,15 @@ function save_options() {
   var redirect = document.getElementById('redirect').checked;
   var goBack =   document.getElementById('goBack').checked;
   var dark =     document.getElementById('dark').checked;
+  var colorOne = document.getElementById('colorPicker1').value;
+  var colorTwo = document.getElementById('colorPicker2').value;
 
   chrome.storage.local.set({
     redirect: redirect,
     goBack:   goBack,
     dark:     dark,
+    colorOne: colorOne;
+    colorTwo: colorTwo;
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -17,7 +21,7 @@ function save_options() {
     }, 750);
   });
 }
-
+ 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
@@ -25,10 +29,14 @@ function restore_options() {
     redirect: true,
     goBack:   true,
     dark:     false,
+    colorOne: #ffffff,
+    colorTwo: #ffffff,
   }, function(items) {
     document.getElementById('redirect').checked		= items.redirect;
     document.getElementById('goBack').checked		= items.goBack;
     document.getElementById('dark').checked		= items.dark;
+    document.getElementById('colorPicker1').value = items.colorOne;
+    document.getElementById('colorPicker1').value = items.colorTwo;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);

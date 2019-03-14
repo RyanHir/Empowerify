10ca142e37635bf601f11d88fc6d7569895dabf5
Ryan H <littlekishi@gmail.com>
2019-03-13 19:34:55 -0500

    salmon

--- src/js/background.js
+++ src/js/background.js
@@ -1,18 +1,27 @@
-var dark     = false;
+var dark	= false;
+var theme	= "dark";
 
 function getData(){
 	chrome.storage.local.get({
-		dark:     false,
+		dark:	false,
+		theme:	"dark"
 	}, function(items) {
-		dark     = items.dark;
+		dark	= items.dark;
+		theme	= items.theme;
 	});
 }
 
 chrome.tabs.onUpdated.addListener(function(tab) {
 	getData();
 	if (dark == true) {
-		chrome.tabs.insertCSS({
-			file: '/src/css/inject/dark.css',
-		}); 
+		if (theme == "dark") {
+			chrome.tabs.insertCSS({
+				file: '/src/css/inject/dark.css',
+			});
+		} else if (theme == "salmon") {
+			chrome.tabs.insertCSS({
+				file: '/src/css/inject/salmon.css',
+			});
+		}
 	}
 });

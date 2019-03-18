279d36a1140c0955b889a08fc4df588926431612
unknown <littlekishi@gmail.com>
2019-03-17 19:51:39 -0500

    ahh

--- src/js/empowerify.js
+++ src/js/empowerify.js
@@ -1,19 +1,16 @@
-var goBack	= false;
+window.onbeforeunload = function() {
+	var goBack	= false;
 
-function getData(){
 	chrome.storage.local.get({
 		goBack:   true,
 	}, function(items) {
 		goBack   = items.goBack;
 	});
-}
-function closingCode(){
-	getData();
+
 	var message = "Are you sure you want to navigate away from this page?\n\nPress OK to continue or Cancel to stay on the current page.";
 	
 	if (goBack == true) {
 		if (confirm(message)) return true;
 		return false;
 	}
-}
-window.onbeforeunload	= closingCode;
+};

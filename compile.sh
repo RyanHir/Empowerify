5a58a0f769444dc24ae99ad93d6c5c53dac6da16
Ryan H <littlekishi@gmail.com>
2019-03-19 13:12:52 -0500

    Ahh firefox needs a seperate build

--- compile.sh
+++ compile.sh
@@ -1,2 +1,7 @@
-echo "compiling version" $(cat manifest.json | jq ".version" | sed 's/"//g')
-zip -r empowerify.zip * -x docs/**\* -x *.sh -x *.md $> /dev/null
+echo "compiling version" $(cat manifest.json | jq ".version" | sed 's/"//g') " For Chrome"
+zip -r chrome.zip * -x docs/**\* -x *.sh -x *.md $> /dev/null
+echo "compiling version" $(cat manifest.json | jq ".version" | sed 's/"//g') " For Firefox"
+mv manifest.json manifestChrome.json
+jq '.permissions += ["tabs"]' manifestChrome.json >> manifest.json
+zip -r firefox.zip * -x docs/**\* -x *.sh -x *.md $> /dev/null
+mv manifestChrome.json manifest.json

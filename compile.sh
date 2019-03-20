1b2b560b45e399849745c7c9cfa50ad385d51c58
Ryan H <littlekishi@gmail.com>
2019-03-19 21:41:47 -0500

    Fixed popup getting cut off

--- compile.sh
+++ compile.sh
@@ -1,7 +1,6 @@
 mkdir -p out/
-echo "compiling version" $(cat manifest.json | jq ".version" | sed 's/"//g') " For Chrome"
+echo "compiling version" $(cat manifest.json | jq ".version" | sed 's/"//g')
 zip -r out/chrome.zip * -x docs/**\* -x *.sh -x *.md $> /dev/null
-echo "compiling version" $(cat manifest.json | jq ".version" | sed 's/"//g') " For Firefox"
 mv manifest.json manifestChrome.json
 jq '.permissions += ["tabs"]' manifestChrome.json >> manifest.json
 zip -r out/firefox.zip * -x docs/**\* -x *.sh -x *.md $> /dev/null

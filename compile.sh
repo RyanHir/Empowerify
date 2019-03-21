9d713750b4d9e4c5c53371ab4766894df23e300b
Ryan Hirasaki <littlekishi@gmail.com>
2019-03-21 12:37:36 -0500

    vjnbrslib

--- compile.sh
+++ compile.sh
@@ -1,7 +1,9 @@
+temp=manifestChrome.json
+
 mkdir -p out/
 echo "compiling version" $(cat manifest.json | jq ".version" | sed 's/"//g')
 zip -r out/chrome.zip * -x docs/**\* -x *.sh -x *.md $> /dev/null
-mv manifest.json manifestChrome.json
-jq '.permissions += ["tabs"]' manifestChrome.json >> manifest.json
-zip -r out/firefox.zip * -x docs/**\* -x *.sh -x *.md $> /dev/null
-mv manifestChrome.json manifest.json
+mv manifest.json $temp
+jq '.permissions += ["tabs"]' $temp >> manifest.json
+zip -r out/firefox.zip * -x docs/**\* -x *.sh -x *.md -x $temp $> /dev/null
+mv $temp manifest.json

ec7c7306cdd3827841b810e8f3d8890651f27c0c
Ryan H <littlekishi@gmail.com>
2019-03-29 20:42:33 -0500

    Removed unused manifest tag from chrome

--- compile.sh
+++ compile.sh
@@ -12,7 +12,12 @@ zip -r "$output/chrome.zip" * -x $exclude $> /dev/null
 
 mv manifest.json $temp
 
-cat $temp | jq '.permissions += ["tabs"]' >> manifest.json
+cat $temp | 
+	jq '.permissions += ["tabs"]' |
+	jq ".applications =
+		{\"gecko\": 
+			{\"update_url\": \"https://ryanhir.github.io/Empowerify/update.json\"}
+		}" >> manifest.json
 zip -r "$output/firefox.zip" * -x $exclude $> /dev/null
 
 mv $temp manifest.json
